c31554640f8a7fc042fade0522a615f0a306baec
goutham rao <goutham.rao@uk.sopragroup.com>
2015-07-20 15:46:39 +0100

    Angular routing set up

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var app = angular.module('flapperNews', []);
-app.factory('posts',[function(){
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider,$urlRouterProvider){
+
+$stateProvider
+	.state('home',{
+		url:'/home',
+		templateUrl: '/home.html',
+		controller: 'MainCtrl'
+	})
+
+$urlRouterProvider.otherwise('home');
+
+}])
+.factory('posts',[function(){
 
 var postStore = {
 	posts: [{title: 'Hello' , link:'helloworld.org', upvotes:9}]
 }
 
 return postStore;
-}]);
-app.controller('MainCtrl', [
-'$scope','posts',
-function($scope,posts){
-  $scope.test = 'Hello world two!';
+}])
+.controller('MainCtrl', [
+'$scope','posts',function($scope,posts){
   
   $scope.posts = posts.posts;
   
@@ -23,7 +36,7 @@ function($scope,posts){
 			});
 		$scope.title = '';
 		$scope.link = '';
-  }
+  } 
   
   $scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
